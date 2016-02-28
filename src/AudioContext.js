95cbf564fa9ec93be14809c96718874b707dc0e9
Chris Bala <chrbala@gmail.com>
2016-02-27 18:54:36 -0800

    Allow for externally provided audioContext

--- src/AudioContext.js
+++ src/AudioContext.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 
-var context = window.AudioContext || window.webkitAudioContext
+var Context = window.AudioContext || window.webkitAudioContext
 
 export default class AudioContextComponent extends Component {
 	componentWillMount() {
-		if (context)
-			this.audioContext = new context()
+		if (this.props.audioContext)
+			return
+
+		if (Context)
+			this.audioContext = new Context()
 		else {
 			console.error('AudioContext not supported in this browser')
 			this.audioContext = {}
@@ -13,11 +16,12 @@ export default class AudioContextComponent extends Component {
 	}
 
 	componentWillUnmount() {
-		this.audioContext.close()
+		if (this.audioContext)
+			this.audioContext.close()
 	}
 
 	getChildContext() {
-		return { audioContext: this.audioContext }
+		return { audioContext: this.props.audioContext || this.audioContext }
 	}
 
 	render() {
